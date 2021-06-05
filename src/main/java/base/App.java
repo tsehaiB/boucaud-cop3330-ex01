84a118efbbaca3985ef3e70b3f8a5ae8c18b09c0
Art3mis <tsehaib@knights.ucf.edu>
2021-06-05 13:31:48 -0400

    initial solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,5 +1,20 @@
 package base;
 
+import java.util.Scanner;
+
 public class App    {
-    //hi
+    public static void main(String[] args)
+    {
+        /* Program creates a new instance of scanner to take in user input
+           Prints a prompt for user input
+           saves user input to variable 'name'
+           puts the name in the greeting
+           and prints out the completed greeting (input, concatenation, and output are seperate)
+         */
+        Scanner inp = new Scanner(System.in);
+        System.out.print("What's your name? ");
+        String name = inp.nextLine();
+        String concatenation = "Hello, " + name + ", nice to meet you!";
+        System.out.println(concatenation);
+    }
 }
